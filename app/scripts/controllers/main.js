87bd5fb7e7c8316c253cac4d2aeb4287ec5a7e89
Jan-Henrik Bruhn <jh.bruhn@me.com>
2014-07-25 22:28:39 +0200

    Added some things.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pedasPageApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pedasPageApp
+ */
+angular.module('pedasPageApp')
+  .controller('MainCtrl', function () {
+  });
